dd1aba427d06af473f3b7ec81289b9c711ef8a74
yi.he <yi.he@weimob.com>
2019-11-24 20:52:13 +0800

    初始化小程序

---
+++ miniprogram/custom-tab-bar/index.js
@@ -0,0 +1,35 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#000",
+    selectedColor: "#E54847",
+    list: [{
+      pagePath: "/pages/index/index",
+      iconPath: "/images/book.png",
+      selectedIconPath: "/images/book-actived.png",
+      text: "首页"
+    }, {
+      add: true,
+      pagePath: "/pages/add/add",
+      text: "发布"
+    }, {
+      pagePath: "/pages/profile/profile",
+      iconPath: "/images/profile.png",
+      selectedIconPath: "/images/profile-actived.png",
+      text: "我的"
+    }]
+  },
+  attached() {},
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({
+        url
+      })
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
