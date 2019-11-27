30d309795a5b446d7da6bb4b488ceca21e680759
yi.he <yi.he@weimob.com>
2019-11-27 11:30:14 +0800

    图片上传

--- miniprogram/pages/add/add.js
+++ miniprogram/pages/add/add.js
@@ -1,11 +1,13 @@
 // miniprogram/pages/add/add.js
+const db = wx.cloud.database()
 Page({
   /**
    * 页面的初始数据
    */
   data: {
     title: '',
-    start: ''
+    start: '',
+    image: ''
   },
   /**
    * 输入标题
@@ -23,15 +25,6 @@ Page({
       start: e.detail.value
     });
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {},
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
@@ -44,28 +37,25 @@ Page({
     }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {}
+  upload: function() {
+    const that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        const tempFilePaths = res.tempFilePaths
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + '.png',
+          filePath: tempFilePaths[0],
+          success: res => {
+            that.setData({
+              image: res.fileID,
+            })
+          },
+          fail: console.error
+        })
+      }
+    })
+  }
 });
