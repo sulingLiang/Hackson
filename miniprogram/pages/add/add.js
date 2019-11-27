68a6f8b735f96bc1b3437e54144efcc1e29d5610
吴国健 <guojian.wu@weimob.com>
2019-11-27 11:31:17 +0800

    1

--- miniprogram/pages/add/add.js
+++ miniprogram/pages/add/add.js
@@ -5,7 +5,8 @@ Page({
    */
   data: {
     title: '',
-    start: ''
+    start: '',
+    imgList: []
   },
   /**
    * 输入标题
@@ -21,6 +22,56 @@ Page({
   startInput(e) {
     this.setData({
       start: e.detail.value
+    });
+  },
+  /**
+   * 选择图片
+   */
+  ChooseImage() {
+    wx.chooseImage({
+      count: 1, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: res => {
+        if (this.data.imgList.length != 0) {
+          this.setData({
+            imgList: this.data.imgList.concat(res.tempFilePaths)
+          });
+        } else {
+          this.setData({
+            imgList: res.tempFilePaths
+          });
+        }
+      }
+    });
+  },
+  /**
+   * 预览图片
+   */
+  ViewImage(e) {
+    console.log(this.data.imgList);
+    wx.previewImage({
+      urls: this.data.imgList,
+      current: e.currentTarget.dataset.url
+    });
+  },
+  /**
+   * 删除图片
+   */
+  DelImg(e) {
+    wx.showModal({
+      title: '梦想家',
+      content: '确定要删除这段回忆吗？',
+      cancelText: '再看看',
+      confirmText: '再见',
+      success: res => {
+        if (res.confirm) {
+          this.data.imgList.splice(e.currentTarget.dataset.index, 1);
+          this.setData({
+            imgList: this.data.imgList
+          });
+        }
+      }
     });
   },
   /**
