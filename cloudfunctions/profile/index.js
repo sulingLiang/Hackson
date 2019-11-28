2aaf9e3e6a578cc2a865ca8e75df58164acc04c7
宋娇娇 <jiaojiao.song@weimob.com>
2019-11-28 21:14:53 +0800

    调整代码

--- cloudfunctions/profile/index.js
+++ cloudfunctions/profile/index.js
@@ -1,32 +1,7 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
-// const getProfileList = async (event, context) => {
-//   return await db.collection(event.db).get().then(
-//     res => {
-//       return {
-//         profileList: res.data
-//       }
-//     }
-//   )
-// }
-// const getDataFromStory = async (event, context) => {
-//   return await db.collection(event.db).where({
-//     _openid: event._openid,
-//     _id: event._id
-//   }).get().then(
-//     res => {
-//       return {
-//         dataList: res.data
-//       }
-//     }
-//   )
-// }
-
-// const funMap = {
-//   getProfileList,
-//   getDataFromStory
-// }
+cloud.init()
+const db = cloud.database()
 
 // 联合查询
 const getDataFromStory = async (event, context) => {
@@ -44,20 +19,13 @@ const getDataFromStory = async (event, context) => {
 
 // 取消点赞
 const cancelLike = async (event, context) => {
-  console.log
   return await db.collection(event.db).doc(event.id).remove()
-  .then(res => {
-    console.log('cancel res', res)
-  })
 }
 
 const funMap = {
   getDataFromStory,
   cancelLike
 }
-
-cloud.init()
-const db = cloud.database()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
